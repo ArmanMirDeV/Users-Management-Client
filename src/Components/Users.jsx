851cd555b6554748dfeb92f6c30935bc2c7eed48
Arman Mir <mirarman8583@gmail.com>
2025-10-28 17:35:48 +0600

    Refactor App component to manage user data and implement Users component

---
+++ src/Components/Users.jsx
@@ -0,0 +1,12 @@
+import React, { use } from 'react';
+
+const Users = ({ usersPromise }) => {
+
+    const users = use(usersPromise);
+    console.log(users);
+    
+
+  return <div></div>;
+};
+
+export default Users;
