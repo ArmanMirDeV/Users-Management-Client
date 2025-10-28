bd4a2795655633882a9fea7be49d5e138ba8fe8b
Arman Mir <mirarman8583@gmail.com>
2025-10-28 17:40:46 +0600

    user displayed

--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -1,12 +1,30 @@
-import React, { use } from 'react';
+import React, { use } from "react";
 
 const Users = ({ usersPromise }) => {
+  const users = use(usersPromise);
+  console.log(users);
 
-    const users = use(usersPromise);
-    console.log(users);
-    
-
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <h3>Add a user: </h3>
+        <form>
+          <input type="text" /> <br />
+          <input type="email" name="" id="" />
+          <br />
+          <button>Add User</button>
+        </form>
+      </div>
+      <div>
+        {users.map((user) => (
+          <p key={user.id}>
+            {user.name} <br /> Email: {user.email}
+          </p>
+        ))}
+        
+      </div>
+    </div>
+  );
 };
 
 export default Users;
